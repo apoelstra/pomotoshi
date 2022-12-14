59a19772e85ae54db4c8d24e01dccfacbd691ca4
Andrew Poelstra <apoelstra@wpsoftware.net>
2022-12-14 00:22:21 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+// Pomotoshi
+// Written in 2022 by
+//   Andrew Poelstra <icboc@wpsoftware.net>
+//
+// To the extent possible under law, the author(s) have dedicated all
+// copyright and related and neighboring rights to this software to
+// the public domain worldwide. This software is distributed without
+// any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication
+// along with this software.
+// If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+//
+
+//! Pomotoshi
+//!
+//! Strongly inspired by [Pomobar](https://github.com/rlcintra/pomobar), this
+//! utility is a Pomodoro timer, designed to be controlled via DBus and to
+//! provide output via xmobar.
+//!
+
+fn main() {
+}
+
